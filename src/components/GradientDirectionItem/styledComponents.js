8398826b0883e1b57971b3ad4a454ef1a82e2ff9
Arthikumari02 <138019511+Arthikumari02@users.noreply.github.com>
2024-07-03 15:30:05 +0530

    Update styledComponents.js

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -1,9 +1,28 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
-export const Button = styled.button`
-  background-color: white;
-  color: #1e293b;
+export const ListItem = styled.li`
+  width: 49%;
+  list-style-type: none;
+  @media screen and (min-width: 768px) {
+    width: 24%;
+  }
+`
+export const DirectionButton = styled.button`
+  text-align: center;
+  color: ${props => (props.isActive ? '#334155' : '#1e293b')};
+  background-color: #ffffff;
+  font-family: 'Roboto';
+  font-size: 14px;
+  font-weight: 600;
+  width: 100%;
   border-radius: 6px;
-  padding: 10px;
-  opacity: ${props => (props.isTabActive ? 1 : 0.5)};
+  border: none;
+  padding-left: 24px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-right: 24px;
+  margin-bottom: 10px;
+  outline: none;
+  opacity: ${props => (props.isActive ? 1 : 0.5)};
+  cursor: pointer;
 `
